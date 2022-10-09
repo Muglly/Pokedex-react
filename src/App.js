9a22aa0fc4c7d3ccc7bb5ae7cc1331a576e9acc9
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-10-08 21:52:31 -0300

    Criação da paginação entre os pokemons

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import Searchbar from './componets/Searchbar';
 
 function App() {
 
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
 
+  const itensPerPage = 27;
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons();
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       });
@@ -21,6 +24,7 @@ function App() {
       const result = await Promise.all(promises);
       setPokemons(result);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage));
     } catch (error) {
       console.log("fetchPokemos error ", error);
     }
@@ -29,13 +33,13 @@ function App() {
   useEffect(() => {
     console.log("Carregou")
     fetchPokemons();
-  }, [])
+  }, [page])
 
   return (
     <div className="App">
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading} />
+      <Pokedex pokemons={pokemons} loading={loading} page={page} setPage={setPage} totalPages={totalPages} />
     </div>
   );
 }
---
+++ src/componets/Pagination.js
@@ -0,0 +1,13 @@
+
+const Pagination = (props) => {
+    const {page, totalPages, onLeftClick, onRightClick} = props
+    return (
+        <div className="pagination-container">
+            <button onClick={onLeftClick}><div>◀</div></button>
+            <div>{page} de {totalPages}</div>
+            <button onClick={onRightClick}><div>▶</div></button>
+        </div>
+    )
+}
+
+export default Pagination;
